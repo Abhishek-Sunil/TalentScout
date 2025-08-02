f6bbc36d80d3c5027f6536bd56d5f793cfeaefc2
Abhishek Sunil <abhisheksunilk108@gmail.com>
2025-08-02 20:47:17 +0530

    pdf text converted in to chunks

--- hirebot.py
+++ hirebot.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 
@@ -10,3 +12,21 @@ with st.sidebar:
     st.title("model training pdf")
     file=st.file_uploader("Upload PDF", type=["pdf"], key="pdf_uploader")
     
+#To extract text from the pdf file 
+if file is not None:
+    pdf_page= PdfReader(file)
+    text=""
+    for page in pdf_page.pages:
+        text += page.extract_text()
+        # st.write(text)
+        
+    #break the text to chunks
+    text_splitter= RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=1000, 
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks= text_splitter.split_text(text)
+    # st.write(chunks)
+# Embedding the text chunks
